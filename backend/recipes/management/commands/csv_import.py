21fa32dac02389221157085c1373e39fe5592925
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-05-29 20:41:11 +0300

    fix

---
+++ backend/recipes/management/commands/csv_import.py
@@ -0,0 +1,34 @@
+import csv
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from recipes.models import IngredientsModel
+
+DATA_ROOT = os.path.join(settings.BASE_DIR, 'recipes/data')
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'filename',
+            default='ingredients.csv',
+            nargs='?',
+            type=str
+        )
+
+    def handle(self, *args, **options):
+        try:
+            with open(os.path.join(DATA_ROOT, options['filename']),
+                      'r', encoding='utf-8') as f:
+                data = csv.reader(f)
+                for row in data:
+                    name, measurement_unit = row
+                    IngredientsModel.objects.get_or_create(
+                        name=name,
+                        measurement_unit=measurement_unit
+                    )
+                print('Load ingredients.csv have successful finished')
+        except FileNotFoundError:
+            raise CommandError('recipes/data/ingredients.csv is not exist')
