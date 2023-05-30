2f5ae8c2881c859003e54873437ad3703b852f73
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-05-30 23:12:54 +0300

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -20,3 +20,4 @@ class UserSerializer(serializers.ModelSerializer):
             'id',
             'is_subscribed'
         )
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import UserViewSet
+from .views import UserViewSet
 
 app_name = "api"
 
 v1_router = DefaultRouter()
 
 v1_router = DefaultRouter()
-v1_router.register("users", UserViewSet)
+v1_router.register("users", UserViewSet, basename='users')
+v1_router.register('ingredients', IngredientViewSet, basename='ingredients')
+v1_router.register('tags', TagViewSet, basename='tags')
+v1_router.register('recipes', RecipeViewSet, 'recipes')
 
 urlpatterns = [
     path("", include(v1_router.urls)),
