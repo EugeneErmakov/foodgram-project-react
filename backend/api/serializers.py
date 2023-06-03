ea8ff66337f10034120ba4edfe4d6cd49d33c372
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-06-03 18:56:13 +0300

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import get_user_model
+import base64
 
+from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 
-from .recipes.models import Tag
-from ..recipes.models import Ingredient, RecipeIngredient
+from ..recipes.models import Ingredient, RecipeIngredient, Tag
 
 UserModel = get_user_model()
 
@@ -52,3 +53,21 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecipeIngredient
         fields = ('amount', 'name', 'measurement_unit', 'id')
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+class RecipeListSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    ingredients = serializers.SerializerMethodField()
+    is_favorited = serializers.BooleanField()
+    is_in_shopping_cart = serializers.BooleanField()
+    image = Base64ImageField()
